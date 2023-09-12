3b27ffe5770ef8d5ab9e0b6d1fbcaa3a4739dd8b
supersonicspeedjj <115861964+supersonicspeedjj@users.noreply.github.com>
2023-09-12 18:00:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+const articles = [];
+const imgsrc = [];
+
+
+app.get("/", async (req, res) => {
+  try {
+    
+    const response = await axios.get(
+      "https://www.imdb.com/search/title/?count=100&title_type=feature,tv_series"
+    );
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('h3.lister-item-header').each(function(){
+      // Find the <a> tag inside the current <h3> element
+      const anchor = $(this).find("a");
+
+      // Get the text and href attributes of the <a> tag
+      const text = anchor.text();
+     // const href = anchor.attr("href");
+     // const img = anchor.find("img").attr("src");
+
+      // Do something with the text and href
+      articles.push({
+        text,
+      });
+    });
+    
+    
+    res.json(articles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
