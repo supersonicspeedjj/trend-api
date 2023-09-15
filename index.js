85206cae6b5b81a6ba954684d20389502bda6054
supersonicspeedjj <115861964+supersonicspeedjj@users.noreply.github.com>
2023-09-15 14:06:14 +0530

    changes

--- index.js
+++ index.js
@@ -37,7 +37,35 @@ app.get("/", async (req, res) => {
     res.json(first25Articles);
   
 });
+app.get("/movies", async (req, res) => {
 
+    
+  const response = await axios.get(
+    "https://editorial.rottentomatoes.com/guide/popular-movies/"
+  );
+  const html = response.data;
+  const $ = cheerio.load(html);
+
+  $("div.article_movie_title").each(function () {
+    // Find the <a> tag inside the current <h3> element
+    const anchor = $(this).find("a");
+
+    // Get the text and href attributes of the <a> tag
+    const text = anchor.text();
+   // const href = anchor.attr("href");
+   // const img = anchor.find("img").attr("src");
+
+    // Do something with the text and href
+    articles.push({
+      text,
+    });
+  });
+  
+  const first25Articles = articles.slice(0, 25);
+  
+  res.json(first25Articles);
+
+});
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
 module.exports=app;
 //if i am going to host this on github pages what changes do i need to make
